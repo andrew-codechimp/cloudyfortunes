8ce62a0d89d81d3eea52480bb91cd550cd1ba9f5
Andrew Jackson <andrew@codechimp.org>
2014-06-05 19:17:28 +0100

    Start of web page content

--- views.py
+++ views.py
@@ -1,5 +1,16 @@
+from apis import CloudyFortunesApi
 import webapp2
+import jinja2
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.redirect('static/index.html')
+        
+        quote = CloudyFortunesApi.QuoteRandom()
+        
+        template_values = {
+            'quote_content': quote.Content,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('static/index.html')
+        self.response.write(template.render(template_values))
