9fe0835f5d6522fb6beadbe11dfb356f34114792
Andrew-Codechimp <andrew@codechimp.org>
2014-06-05 15:50:33 +0100

    Added bootstrap and static index page

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import webapp2
+
 from google.appengine.ext import endpoints
 from apis import CloudyFortunesApi
+from views import MainPage
 
 # Use of endpoints.api_server is the same for APIs created with or without
 # endpoints-proto-datastore.
 api = endpoints.api_server([CloudyFortunesApi], restricted=False)
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
