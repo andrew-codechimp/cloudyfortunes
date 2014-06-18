2cb928fe3656825259bc9b7c484293639901546a
Andrew-Codechimp <andrew@codechimp.org>
2014-06-18 13:18:44 +0100

    Fix

--- views.py
+++ views.py
@@ -26,7 +26,7 @@ class MainPage(webapp2.RequestHandler):
         cf = CloudyFortunesApi()
 
         template_values = {    
-            'quote':  cf.QuoteRandom(Quote.query()),
+            'quote':  cf.QuoteRandom(Query(quote())),
         }
 
         template = JINJA_ENVIRONMENT.get_template('index.html')
